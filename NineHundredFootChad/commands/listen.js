655f7ce2baacf75ca5abada207662b8791e61b1d
CJ Johnson <mtnkodiak@pobox.com>
2020-07-23 17:16:21 -0600

    introduced listen command

--- NineHundredFootChad/commands/listen.js
+++ NineHundredFootChad/commands/listen.js
@@ -11,7 +11,28 @@ module.exports = {
 			return;
 		}
 
-		
+		// Voice only works in guilds, if the message does not come from a guild,
+		// we ignore it
+		if (!message.guild) return;
+
+		// Only try to join the sender's voice channel if they are in one themselves
+		if (message.member.voice.channel) {
+			const connection = await message.member.voice.channel.join();
+
+			if (toggle = "on") {
+				
+			} else { //toggle = "off"
+
+			}
+
+
+			this.playChad(connection, reps);
+
+		} else {
+			message.reply('You need to join a voice channel first!');
+		}
+
+
 
 	}
 }
